0859727c0d526e0089605cd5e4338b1c2c64f479
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-11-21 17:51:20 +0100

    Fixed Bugs

--- elements/javascript/images.js
+++ elements/javascript/images.js
@@ -1,4 +1,3 @@
-
 // Big Images
 function onClick(element) {
 	document.getElementById("img").src = element.src;
@@ -10,6 +9,6 @@ function onClick(element) {
 /// fix broken images / lazy loading
 var images = document.getElementsByTagName("img");
 for (i = 0; i < images.length; i++) {
-	images[i].onerror = function() {this.onerror=null;this.src="https://rollmaterial-rhb.netlify.app/elements/noimage.png";}
+	images[i].onerror = function() {this.onerror=null;this.src="https://prmodellbau.netlify.app/elements/pictures/noimage.png";}
 	images[i].loading = "lazy";
 }
