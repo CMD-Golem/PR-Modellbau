2d94cd78d45ae4746454787f04bc9f08c1fecb58
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-01-23 19:40:11 +0100

    Added new index, new pictures, new nav, products

--- elements/javascript/image_gallery.js
+++ elements/javascript/image_gallery.js
@@ -1,42 +1,49 @@
 var slideIndex = 1;
+var used_slide = false;
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
+	used_slide = true;
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
+	used_slide = true;
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slideshow");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
+	var i;
+	var slides = document.getElementsByClassName("slideshow");
+	if (n > slides.length) {slideIndex = 1}		
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";	
+	}
+	slides[slideIndex-1].style.display = "block";	
 }
 
 
 // Show Pictures without text in front
 function bigView() {
-  var body = document.getElementsByTagName("body")[0];
+	var body = document.getElementsByTagName("body")[0];
 
-  if (body.classList.contains("big_view") == false) {
-    body.classList.add("big_view");
+	if (body.classList.contains("big_view") == false) {
+		body.classList.add("big_view");
+	}
+	else {
+		body.classList.remove("big_view");
+	}
+}
 
-    document.getElementsByTagName("aside")[0].style.left = "0";
-    document.getElementById("filter_close").style.display = "block";
-  }
 
-  else {
-    body.classList.remove("big_view");
+// Change images if user hasn't clicked to change slide
+setTimeout(autoSlides, 5000);
 
-    document.getElementsByTagName("aside")[0].style.left = "-401px";
-    document.getElementById("filter_close").style.display = "none";
-  }
-  
+function autoSlides() {
+	if (used_slide == false) {
+		setTimeout(autoSlides, 5000);
+		showSlides(slideIndex += 1);
+	}
 }
